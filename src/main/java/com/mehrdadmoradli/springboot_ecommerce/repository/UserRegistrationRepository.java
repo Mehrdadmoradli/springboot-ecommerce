99588800c25ec77c48967cfe3f0df449b8f2fbe7
Mehrdad Moradli <MeM@dig.local>
2026-03-05 16:49:42 +0100

    Add User Registration Repository

---
+++ src/main/java/com/mehrdadmoradli/springboot_ecommerce/repository/UserRegistrationRepository.java
@@ -0,0 +1,13 @@
+package com.mehrdadmoradli.springboot_ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mehrdadmoradli.springboot_ecommerce.entity.User;
+import java.util.Optional;
+
+public interface UserRegistrationRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByUsername(String username);
+	Optional<User> findByEmail(String email);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
+}
