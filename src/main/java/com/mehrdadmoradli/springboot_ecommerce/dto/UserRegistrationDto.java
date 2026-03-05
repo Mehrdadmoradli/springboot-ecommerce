ce707be9b40df052dfcd36ce0f1c1aeab7b893ff
Mehrdad Moradli <MeM@dig.local>
2026-03-05 16:32:42 +0100

    Add Dto Classes

---
+++ src/main/java/com/mehrdadmoradli/springboot_ecommerce/dto/UserRegistrationDto.java
@@ -0,0 +1,38 @@
+package com.mehrdadmoradli.springboot_ecommerce.dto;
+
+public class UserRegistrationDto {
+
+    private String email;
+    private String username;
+    private String password;
+
+    public UserRegistrationDto() {
+
+    }
+
+    public UserRegistrationDto(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
