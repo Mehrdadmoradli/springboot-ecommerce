7cbc4a0fcd98a1f3b2375b361ec2440a458011dd
Mehrdad Moradli <117904723+Mehrdadmoradli@users.noreply.github.com>
2026-03-23 23:10:41 +0100

    Add Order and OrderItem classes

---
+++ src/main/java/com/mehrdadmoradli/springboot_ecommerce/entity/Order.java
@@ -0,0 +1,80 @@
+package com.mehrdadmoradli.springboot_ecommerce.entity;
+
+import jakarta.persistence.*;
+import java.math.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	@Column(nullable = false)
+	private BigDecimal totalPrice;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+	private List<OrderItem> items = new ArrayList<>();
+	
+	
+	public Order() {
+	}
+
+	public Order(User user, BigDecimal totalPrice, LocalDateTime createdAt, List<OrderItem> items) {
+		this.user = user;
+		this.totalPrice = totalPrice;
+		this.createdAt = createdAt;
+		this.items = items;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(BigDecimal totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+	
+	@PrePersist
+	public void setCreatedAt() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+}
